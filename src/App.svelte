03dc8bd16fa12f19a829ea9e820bcec78f67d9b8
Maxime <redval@gmail.com>
2023-07-16 12:05:07 +0200

    no normalize and no smooth

--- src/App.svelte
+++ src/App.svelte
@@ -14,13 +14,13 @@
     import { onScrollAssets, typographicPaths, animAssets, typographicScrollProgress  } from './propassets';
     import gsap from 'gsap';
     import ScrollTrigger from 'gsap/ScrollTrigger';
-    import ScrollSmoother from './lib/ScrollSmoother';
+    // import ScrollSmoother from './lib/ScrollSmoother';
     gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-    ScrollSmoother.create({
-        smooth: 1,               // how long (in seconds) it takes to "catch up" to the native scroll position
-        effects: true,           // looks for data-speed and data-lag attributes on elements
-        smoothTouch: 0.1,        // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
-    });
+    // ScrollSmoother.create({
+    //     smooth: 1,               // how long (in seconds) it takes to "catch up" to the native scroll position
+    //     effects: true,           // looks for data-speed and data-lag attributes on elements
+    //     smoothTouch: 0.1,        // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+    // });
     ScrollTrigger.clearScrollMemory( ) ;
     ScrollTrigger.config({limitCallbacks: true });
 
@@ -43,8 +43,8 @@
         document.body.style.background='white';
         onScrolls = onScrollAssets(elementsCheesy, isMobile);
         anims = animAssets(echoes);
-        await tick();
-        ScrollSmoother.normalizeScroll = true;
+        // await tick();
+        // ScrollSmoother.normalizeScroll = true;
     })
 
     const resizeObserver = new ResizeObserver(() => isMobile = window.matchMedia("(orientation : portrait), (max-width : 675px)").matches);
