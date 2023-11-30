158afd0b9657649e225f0f52dae36b18560d99ba
Maxime <redval@gmail.com>
2023-11-30 11:36:16 +0100

    better revert

--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
-import App from "./App.svelte";
-new App({ target: document.documentElement });
+import './app.css'
+import App from './App.svelte'
+
+const app = new App({
+  target: document.getElementById('app'),
+})
+
+export default app
