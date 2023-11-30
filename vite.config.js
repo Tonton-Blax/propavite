9120365c3b10a153bf77d366b9d7b4df79e60c01
Maxime <redval@gmail.com>
2023-11-30 10:34:42 +0100

    retest

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { resolve } from 'path'
-import dts from 'vite-plugin-dts'
+//import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(),dts({ include: ['lib'] })],
-  build: {
-    copyPublicDir: false,
-    lib: {
-      entry: resolve(__dirname, 'src/lib/main.js'),
-      formats: ['es']
-    }
-  }
+  plugins: [svelte()],
+  // build: {
+  //   copyPublicDir: false,
+  //   lib: {
+  //     entry: resolve(__dirname, 'src/lib/main.js'),
+  //     formats: ['es']
+  //   }
+  // }
 })
