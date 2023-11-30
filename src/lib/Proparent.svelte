0413c48eb952edaf18a4d7dd1c5b3bcb8a178ae6
Maxime <redval@gmail.com>
2023-11-30 11:17:18 +0100

    Revert "typo fix"
    This reverts commit 908729d21cdad988cd58749a5c8638d883c45208.

--- src/lib/Proparent.svelte
+++ src/lib/Proparent.svelte
@@ -73,7 +73,6 @@
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<!-- svelte-ignore a11y-no-static-element-interactions -->
 <div id={id} class="propaganda-wrapper"
     bind:this={container}
     class:proparent-gradient={!!gradient}
